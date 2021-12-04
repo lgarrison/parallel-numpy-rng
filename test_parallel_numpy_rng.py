50380351d1b0906e74946c3b411c0c05ca813cc5
Lehman Garrison <lgarrison@flatironinstitute.org>
2021-12-03 22:24:46 -0500

    fix typo in tests

--- test_parallel_numpy_rng.py
+++ test_parallel_numpy_rng.py
@@ -130,7 +130,7 @@ def test_uniform_matches_numpy(someN, seed, nthread, dtype):
         b = rng.random(size=N, dtype=dtype)
         
         if dtype == np.float64:
-            assert np.allclose(s, p, atol=1e-15, rol=0.)
+            assert np.allclose(a, b, atol=1e-15, rol=0.)
         elif dtype == np.float32:
             assert np.allclose(a, b, atol=1e-7)
         else:
